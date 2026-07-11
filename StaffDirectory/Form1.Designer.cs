3f0754b265eb65d2a1dd6a3d6ebea4f0c38fafdc
Kaweesh Ravishanka Liyanage <kaweeshravishanka8@gmail.com>
2026-07-11 13:31:09 +0530

    Add more Desing

--- StaffDirectory/Form1.Designer.cs
+++ StaffDirectory/Form1.Designer.cs
@@ -28,13 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             menuStrip1 = new MenuStrip();
-            toolStrip1 = new ToolStrip();
-            lblStatus = new StatusStrip();
-            toolStripStatusLabel1 = new ToolStripStatusLabel();
-            panel1 = new Panel();
-            txtSearch = new Button();
-            btnSearch = new TextBox();
             fileToolStripMenuItem = new ToolStripMenuItem();
             newToolStripMenuItem = new ToolStripMenuItem();
             openToolStripMenuItem = new ToolStripMenuItem();
@@ -47,7 +42,19 @@
             statusBarToolStripMenuItem = new ToolStripMenuItem();
             helpToolStripMenuItem = new ToolStripMenuItem();
             aboutToolStripMenuItem = new ToolStripMenuItem();
+            toolStrip1 = new ToolStrip();
+            btnToolbarAdd = new ToolStripButton();
+            btnToolbarRemove = new ToolStripButton();
+            btnToolbarSave = new ToolStripButton();
+            btnToolbarSearch = new ToolStripButton();
+            lblStatus = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            panel1 = new Panel();
+            txtSearch = new Button();
+            btnSearch = new TextBox();
+            lstStaff = new ListBox();
             menuStrip1.SuspendLayout();
+            toolStrip1.SuspendLayout();
             lblStatus.SuspendLayout();
             panel1.SuspendLayout();
             SuspendLayout();
@@ -62,14 +69,130 @@
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
             // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newToolStripMenuItem, openToolStripMenuItem, saveAsToolStripMenuItem, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            newToolStripMenuItem.Name = "newToolStripMenuItem";
+            newToolStripMenuItem.Size = new Size(143, 26);
+            newToolStripMenuItem.Text = "New";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(143, 26);
+            openToolStripMenuItem.Text = "Opne";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            saveAsToolStripMenuItem.Size = new Size(143, 26);
+            saveAsToolStripMenuItem.Text = "Save As";
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(143, 26);
+            exitToolStripMenuItem.Text = "Exit";
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { clearAllToolStripMenuItem, customizeToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(49, 24);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // clearAllToolStripMenuItem
+            // 
+            clearAllToolStripMenuItem.Name = "clearAllToolStripMenuItem";
+            clearAllToolStripMenuItem.Size = new Size(161, 26);
+            clearAllToolStripMenuItem.Text = "Clear All";
+            // 
+            // customizeToolStripMenuItem
+            // 
+            customizeToolStripMenuItem.Name = "customizeToolStripMenuItem";
+            customizeToolStripMenuItem.Size = new Size(161, 26);
+            customizeToolStripMenuItem.Text = "Customize";
+            // 
+            // viweToolStripMenuItem
+            // 
+            viweToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { statusBarToolStripMenuItem });
+            viweToolStripMenuItem.Name = "viweToolStripMenuItem";
+            viweToolStripMenuItem.Size = new Size(55, 24);
+            viweToolStripMenuItem.Text = "View";
+            // 
+            // statusBarToolStripMenuItem
+            // 
+            statusBarToolStripMenuItem.Checked = true;
+            statusBarToolStripMenuItem.CheckOnClick = true;
+            statusBarToolStripMenuItem.CheckState = CheckState.Checked;
+            statusBarToolStripMenuItem.Name = "statusBarToolStripMenuItem";
+            statusBarToolStripMenuItem.Size = new Size(158, 26);
+            statusBarToolStripMenuItem.Text = "Status Bar";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aboutToolStripMenuItem });
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(55, 24);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(133, 26);
+            aboutToolStripMenuItem.Text = "About";
+            // 
             // toolStrip1
             // 
             toolStrip1.ImageScalingSize = new Size(20, 20);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { btnToolbarAdd, btnToolbarRemove, btnToolbarSave, btnToolbarSearch });
             toolStrip1.Location = new Point(0, 28);
             toolStrip1.Name = "toolStrip1";
-            toolStrip1.Size = new Size(984, 25);
+            toolStrip1.Size = new Size(984, 27);
             toolStrip1.TabIndex = 1;
             toolStrip1.Text = "toolStrip1";
+            // 
+            // btnToolbarAdd
+            // 
+            btnToolbarAdd.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnToolbarAdd.Image = (Image)resources.GetObject("btnToolbarAdd.Image");
+            btnToolbarAdd.ImageTransparentColor = Color.Magenta;
+            btnToolbarAdd.Name = "btnToolbarAdd";
+            btnToolbarAdd.Size = new Size(41, 24);
+            btnToolbarAdd.Text = "Add";
+            // 
+            // btnToolbarRemove
+            // 
+            btnToolbarRemove.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnToolbarRemove.Image = (Image)resources.GetObject("btnToolbarRemove.Image");
+            btnToolbarRemove.ImageTransparentColor = Color.Magenta;
+            btnToolbarRemove.Name = "btnToolbarRemove";
+            btnToolbarRemove.Size = new Size(67, 24);
+            btnToolbarRemove.Text = "Remove";
+            // 
+            // btnToolbarSave
+            // 
+            btnToolbarSave.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnToolbarSave.Image = (Image)resources.GetObject("btnToolbarSave.Image");
+            btnToolbarSave.ImageTransparentColor = Color.Magenta;
+            btnToolbarSave.Name = "btnToolbarSave";
+            btnToolbarSave.Size = new Size(44, 24);
+            btnToolbarSave.Text = "Save";
+            // 
+            // btnToolbarSearch
+            // 
+            btnToolbarSearch.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnToolbarSearch.Image = (Image)resources.GetObject("btnToolbarSearch.Image");
+            btnToolbarSearch.ImageTransparentColor = Color.Magenta;
+            btnToolbarSearch.Name = "btnToolbarSearch";
+            btnToolbarSearch.Size = new Size(57, 24);
+            btnToolbarSearch.Text = "Search";
             // 
             // lblStatus
             // 
@@ -112,90 +235,20 @@
             btnSearch.Size = new Size(559, 27);
             btnSearch.TabIndex = 0;
             // 
-            // fileToolStripMenuItem
+            // lstStaff
             // 
-            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newToolStripMenuItem, openToolStripMenuItem, saveAsToolStripMenuItem, exitToolStripMenuItem });
-            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            fileToolStripMenuItem.Size = new Size(46, 24);
-            fileToolStripMenuItem.Text = "File";
-            // 
-            // newToolStripMenuItem
-            // 
-            newToolStripMenuItem.Name = "newToolStripMenuItem";
-            newToolStripMenuItem.Size = new Size(224, 26);
-            newToolStripMenuItem.Text = "New";
-            // 
-            // openToolStripMenuItem
-            // 
-            openToolStripMenuItem.Name = "openToolStripMenuItem";
-            openToolStripMenuItem.Size = new Size(224, 26);
-            openToolStripMenuItem.Text = "Opne";
-            // 
-            // saveAsToolStripMenuItem
-            // 
-            saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            saveAsToolStripMenuItem.Size = new Size(224, 26);
-            saveAsToolStripMenuItem.Text = "Save As";
-            // 
-            // exitToolStripMenuItem
-            // 
-            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            exitToolStripMenuItem.Size = new Size(224, 26);
-            exitToolStripMenuItem.Text = "Exit";
-            // 
-            // editToolStripMenuItem
-            // 
-            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { clearAllToolStripMenuItem, customizeToolStripMenuItem });
-            editToolStripMenuItem.Name = "editToolStripMenuItem";
-            editToolStripMenuItem.Size = new Size(49, 24);
-            editToolStripMenuItem.Text = "Edit";
-            // 
-            // clearAllToolStripMenuItem
-            // 
-            clearAllToolStripMenuItem.Name = "clearAllToolStripMenuItem";
-            clearAllToolStripMenuItem.Size = new Size(224, 26);
-            clearAllToolStripMenuItem.Text = "Clear All";
-            // 
-            // customizeToolStripMenuItem
-            // 
-            customizeToolStripMenuItem.Name = "customizeToolStripMenuItem";
-            customizeToolStripMenuItem.Size = new Size(224, 26);
-            customizeToolStripMenuItem.Text = "Customize";
-            // 
-            // viweToolStripMenuItem
-            // 
-            viweToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { statusBarToolStripMenuItem });
-            viweToolStripMenuItem.Name = "viweToolStripMenuItem";
-            viweToolStripMenuItem.Size = new Size(55, 24);
-            viweToolStripMenuItem.Text = "View";
-            // 
-            // statusBarToolStripMenuItem
-            // 
-            statusBarToolStripMenuItem.Checked = true;
-            statusBarToolStripMenuItem.CheckOnClick = true;
-            statusBarToolStripMenuItem.CheckState = CheckState.Checked;
-            statusBarToolStripMenuItem.Name = "statusBarToolStripMenuItem";
-            statusBarToolStripMenuItem.Size = new Size(224, 26);
-            statusBarToolStripMenuItem.Text = "Status Bar";
-            // 
-            // helpToolStripMenuItem
-            // 
-            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aboutToolStripMenuItem });
-            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            helpToolStripMenuItem.Size = new Size(55, 24);
-            helpToolStripMenuItem.Text = "Help";
-            // 
-            // aboutToolStripMenuItem
-            // 
-            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            aboutToolStripMenuItem.Size = new Size(224, 26);
-            aboutToolStripMenuItem.Text = "About";
+            lstStaff.FormattingEnabled = true;
+            lstStaff.Location = new Point(0, 55);
+            lstStaff.Name = "lstStaff";
+            lstStaff.Size = new Size(984, 444);
+            lstStaff.TabIndex = 4;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(984, 575);
+            Controls.Add(lstStaff);
             Controls.Add(panel1);
             Controls.Add(lblStatus);
             Controls.Add(toolStrip1);
@@ -206,6 +259,8 @@
             Text = "StaffDirectory";
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
             lblStatus.ResumeLayout(false);
             lblStatus.PerformLayout();
             panel1.ResumeLayout(false);
@@ -235,5 +290,10 @@
         private ToolStripMenuItem statusBarToolStripMenuItem;
         private ToolStripMenuItem helpToolStripMenuItem;
         private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripButton btnToolbarAdd;
+        private ToolStripButton btnToolbarRemove;
+        private ToolStripButton btnToolbarSave;
+        private ToolStripButton btnToolbarSearch;
+        private ListBox lstStaff;
     }
 }
