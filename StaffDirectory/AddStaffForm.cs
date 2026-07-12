abcdb90a5a450d8a3ebfe5d6e78f624500e9202a
Kaweesh Ravishanka Liyanage <kaweeshravishanka8@gmail.com>
2026-07-12 16:14:15 +0530

    Add AddStaffForm

---
+++ StaffDirectory/AddStaffForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StaffDirectory
+{
+    public partial class AddStaffForm : Form
+    {
+        public AddStaffForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
