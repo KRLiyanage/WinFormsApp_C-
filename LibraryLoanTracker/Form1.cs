77d1af976325091e3edcb79f08a00b456fc30a42
Kaweesh Ravishanka Liyanage <kaweeshravishanka8@gmail.com>
2026-07-14 11:56:03 +0530

    Main Form Backing Store

--- LibraryLoanTracker/Form1.cs
+++ LibraryLoanTracker/Form1.cs
@@ -2,6 +2,16 @@ namespace LibraryLoanTracker
 {
     public partial class Form1 : Form
     {
+
+        private readonly List<LoanRecord> _loans = new();
+        private readonly string _dataFilePath =
+        Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.
+        ApplicationData),
+        "LibraryLoanTracker",
+        "loans.json"
+        );
+
         public Form1()
         {
             InitializeComponent();
