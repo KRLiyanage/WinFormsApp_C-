0071c3d03bf2d8e6cb8214a09abeb46bb54dcd1b
Kaweesh Ravishanka Liyanage <kaweeshravishanka8@gmail.com>
2026-07-14 10:00:09 +0530

    Add a new class file LoanRecord.cs

---
+++ LibraryLoanTracker/Form1.cs
@@ -0,0 +1,10 @@
+namespace LibraryLoanTracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LibraryLoanTracker/LoanRecord.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryLoanTracker
+{
+    public class LoanRecord
+    {
+        public string LoanId { get; set; }
+        public string BookTitle { get; set; }
+        public string BorrowerName {  get; set; }
+        public string BorrowerType { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsReturned { get; set; } = false;
+        public bool IsOverdue => !IsReturned && DateTime.Today > DueDate;
+        public string DisplayText =>$"[{LoanId}] {BookTitle}-- {BorrowerName} ({BorrowerType}) | Due: {DueDate:dd MMM yyyy}{(IsOverdue ? " OVERDUE" : "")}{(IsReturned ?" RETURNED" : "")}";
+
+    }
+}
